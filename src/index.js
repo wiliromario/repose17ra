1d1539b8bec664bd3efe4aec592a5d13eed99659
wiliother <wiliromarioother@gmail.com>
2021-03-17 16:59:54 +0700

    setup sentry

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+Sentry.init({
+  dsn: "https://18dc84e5d4ae415e8d63a8dd62b04a8e@o553394.ingest.sentry.io/5680559",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
