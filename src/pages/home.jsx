ce201a3a8fe61334aec38eac8f3c6e08777b61b3
wiliother <wiliromarioother@gmail.com>
2021-03-17 13:29:46 +0700

    create home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+
+    const [hoverIn, setHoverIn] = useState(0)
+
     return(
-        <div>
-            Apa aja
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            height:"100vh",
+        }}>
+            <ListGroup as="ul">
+                <Link to = '/1'>
+                    <ListGroup.Item as="li"
+                    style = {{cursor: "pointer", width: "100px"}}
+                    active = { hoverIn === 1 ? true : false }
+                    onMouseEnter = {() => setHoverIn(1)}
+                    onMouseLeave = {() => setHoverIn(0)}>
+                        Tes 1
+                    </ListGroup.Item>
+                </Link>
+                <Link to = '/3'>
+                    <ListGroup.Item as="li"
+                    style = {{cursor: "pointer", width: "100px"}}
+                    active = { hoverIn === 3 ? true : false }
+                    onMouseEnter = {() => setHoverIn(3)}
+                    onMouseLeave = {() => setHoverIn(0)}>
+                        Tes 3
+                    </ListGroup.Item>
+                </Link>
+                <Link to = '/5'>
+                    <ListGroup.Item as="li"
+                    style = {{cursor: "pointer", width: "100px"}}
+                    active = { hoverIn === 5 ? true : false }
+                    onMouseEnter = {() => setHoverIn(5)}
+                    onMouseLeave = {() => setHoverIn(0)}>
+                        Tes 5
+                    </ListGroup.Item>
+                </Link>
+            </ListGroup>
         </div>
     )
 }
